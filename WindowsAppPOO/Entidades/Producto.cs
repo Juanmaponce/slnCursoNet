9c861cc1c91c3b8b49df3f016a2160fa06448e17
Juanmaponce <65909673+Juanmaponce@users.noreply.github.com>
2022-10-31 12:41:05 -0300

    Add project files.

---
+++ WindowsAppPOO/Entidades/Producto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsAppPOO.Entidades
+{
+    public class Producto
+    {
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public decimal PrecioCosto { get; set; }
+        public double Margen { get; set; }
+        public double IVA { get; set; }
+        public string Provedor { get; set; }
+        public string Categoria { get; set; }
+        public string SubCategoria { get; set; }
+        public decimal PrecioBruto { get { return PrecioCosto + Convert.ToDecimal(this.Margen); } }
+        public decimal PrecioVenta { get { return PrecioBruto * Convert.ToDecimal(1 + this.IVA); } }
+    }
+}
